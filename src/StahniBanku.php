09c8d7f91a5b62072d08c1ef3fad3b12b5190337
Vítězslav Dvořák <info@vitexsoftware.cz>
2019-05-27 00:15:51 +0200

    Log banking account pull results

--- src/StahniBanku.php
+++ src/StahniBanku.php
@@ -8,10 +8,17 @@ define('EASE_APPNAME', 'StahniBanku');
 require_once '../vendor/autoload.php';
 $shared = new Ease\Shared();
 $shared->loadConfig('../client.json', true);
+$shared->loadConfig('../matcher.json', true);
 
 $banker = new \FlexiPeeHP\Banka();
 $banker->logBanner(constant('EASE_APPNAME'));
 $banker->addStatusMessage(_('Download online bank statements'));
-if (!$banker->stahnoutVypisyOnline()) {
-    $banker->addStatusMessage('Bank Offline!', 'error');
+if ($banker->stahnoutVypisyOnline()) {
+    foreach (explode("\n", $banker->lastCurlResponse) as $row) {
+        if (!empty(trim($row))) {
+            $banker->addStatusMessage($row,'success');
+        }
+    }
+} else {
+    $banker->addStatusMessage('Bank Failure!', 'error');
 }
